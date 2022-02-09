c05e0dcfd3191cf64d257735b6b6ed24542e8f3a
Henry Zarza <Henry.Zarza@walmartlabs.com>
2022-02-09 18:26:32 -0500

    Creating homepage container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
+import styled from 'styled-components';
+import tw from "twin.macro";
+
+import HomePage from './app/containers/HomePage';
+
+const AppContainer = styled.div`
+  ${tw`
+    w-full
+    h-full
+    flex
+    flex-col
+  `}
+`;
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <AppContainer>
+      <HomePage />
+    </AppContainer>
   );
 }
 
